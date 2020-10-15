d4e9c9f627fea36ea78ba664e65e3e382e9aa389
Kristiyan <37977687+404carrynotfound@users.noreply.github.com>
2020-10-15 13:42:36 +0300

    Create problem-4.cpp

---
+++ Exercise 1/Theory/problem-4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    /* 
+    
+    a)
+	
+	int p;
+
+	cout << "p = ";
+
+	cin >> p;
+
+    
+    if (p % 4 == 0 || p % 7 == 0)
+	{
+		cout << "True";
+	}
+	else
+	{
+		cout << "False";
+	}
+    */
+
+
+
+	return 0;
+}
