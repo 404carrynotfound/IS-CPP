dcb57688ebb167c407d4674bd124f000a3323d60
Kristiyan <37977687+404carrynotfound@users.noreply.github.com>
2020-10-15 14:53:00 +0300

    Update problem-4.cpp

--- Exercise 1/Practice/problem-4.cpp
+++ Exercise 1/Practice/problem-4.cpp
@@ -6,11 +6,17 @@ int main()
 	int a, b;
     cout << "Enter 2 numbers";
     cin >> a >> b;
-
+	
+	
 	a = a + b;
 	b = a - b;
 	a = a - b;
 	
+	/*
+	a = a * b;
+	b = a / b;
+	a = a / b;
+	*/
 	cout << "a = " << a << endl;
 	cout << "b = " << b << endl;
 
